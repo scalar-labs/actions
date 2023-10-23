c818517d423f94d0a80f6a2837028916c1a72d66
satoshi hikida <satoshi.hikida@scalar-labs.com>
2023-10-23 14:21:12 +0900

    add comment

--- release-note-script/src/main/java/ReleaseNoteCreation.java
+++ release-note-script/src/main/java/ReleaseNoteCreation.java
@@ -232,7 +232,7 @@ public class ReleaseNoteCreation {
 
     public static Category fromDisplayName(String name) {
       return Arrays.stream(Category.values())
-          .filter(v -> v.name().equalsIgnoreCase(name))
+          .filter(v -> v.name().equalsIgnoreCase(name)) // v.name() is needed to compare with a label of a PR
           .findFirst()
           .orElseThrow(() -> new IllegalArgumentException("Invalid name: " + name));
     }
